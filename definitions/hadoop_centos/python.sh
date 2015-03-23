8be25918f95a3884d6aa3a8c11ac89e902575eee
David Chen <mvjome@gmail.com>
2015-03-23 10:27:43 +0800

    Upgrade Python version to 2.7.9

--- definitions/hadoop_centos/python.sh
+++ definitions/hadoop_centos/python.sh
@@ -12,17 +12,18 @@ if [ ! -d .pyenv ]; then
   echo 'export PIP_DOWNLOAD_CACHE=\$HOME/.pip_download_cache' >> .bash_profile
   echo 'export PYENV_ROOT="\$HOME/.pyenv"' >> .bash_profile
   echo 'export PATH="\$PYENV_ROOT/bin:\$PATH"' >> .bash_profile
-  echo 'export PYENV_VERSION=2.7.5' >> .bash_profile
+  echo 'export PYENV_VERSION=2.7.9' >> .bash_profile
   echo 'eval "\$(pyenv init -)"' >> .bash_profile
 fi
 
 # 安装python2.7.5
 source .bash_profile
-if [ ! -f $HOME/.pyenv/versions/2.7.5 ]; then
+if [ ! -f $HOME/.pyenv/versions/2.7.9 ]; then
   # http://fduo.org/pyenv-mirrow/
   export PYTHON_BUILD_MIRROR_URL="http://pyenv.qiniudn.com/pythons/"
-  pyenv install --keep --verbose 2.7.5
-  pyenv shell 2.7.5
+  # CFLAGS="-I$(xcrun --show-sdk-path)/usr/include" # https://github.com/yyuu/pyenv/wiki/Common-build-problems # find the zlib headers
+  pyenv install --keep --verbose 2.7.9
+  pyenv shell 2.7.9
   pyenv rehash
 fi
 
